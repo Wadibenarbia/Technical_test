a1009203c559589db332d386c9ea245ef626c2d8
wadi <36459068+Wadibenarbia@users.noreply.github.com>
2018-04-24 19:38:34 +0200

    Update cube.js

--- cube.js
+++ cube.js
@@ -1,11 +1,3 @@
-Nous avons un cube composé de petits cubes (ex : un rubiks cube).
-Soit n le nombre de petits cubes dans une arrête du gran cube.
-(ex: pour un rubiks cube classique n = 3).
-
-On veut calculer le nombre N de petits cubes à la périphérie du grand cube.
-(ex pour n = 2, N = 8).
-
-
 var n = 4
 var cube_face = Math.pow(n, 2) = 16
 var cube_total = Math.pow(n, 3) = 64
